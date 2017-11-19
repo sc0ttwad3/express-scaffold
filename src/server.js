bb33111694a12957f5ce94fed8caf0c0b07ee0ef
Scott Wade <sc0ttwad3@gmail.com>
2017-11-18 21:08:59 -0500

    Simplify async server startup

--- src/server.js
+++ src/server.js
@@ -6,13 +6,17 @@ const app = require('./app');
 
 const port = process.env.PORT || '3000';
 
-// Boostrap Express server
-(async function start() {
+/*
+ *  Boostrap Express server
+ */
+async function bootstrap() {
   const server = await http.createServer(app);
   //server.on('error', onError);
   //server.on('listening', onListening);
-  server.listen(
+  await server.listen(
     port,
     () => console.log(chalk.bold.blue(`Express server listening on ${port} ...`))
   );
-})();
+}
+
+bootstrap();
