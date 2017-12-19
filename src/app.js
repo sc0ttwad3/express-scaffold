c86f16ab73aaec951b257a5621492ea43362718b
Scott Wade <sc0ttwad3@gmail.com>
2017-12-18 22:46:59 -0500

    Adjust app.js with querystring and moment, add error not found.

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
+const querystring = require('querystring')
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const sassMiddleware = require('node-sass-middleware');
 const session = require('express-session');
 const uuid = require('uuid/v4');
+const moment = require('moment');
 
 const FileStore = require('session-file-store')(session);
 
@@ -24,7 +26,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 // no longer needed for express-session to work
-// app.use(cookieParser());
+app.use(cookieParser());
 app.use(sassMiddleware({
   src: path.join(__dirname, '../public'),
   dest: path.join(__dirname, '../public'),
@@ -54,8 +56,8 @@ const users = require('./routes/users');
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
+app.use((err, req, res, next) => {
+  console.log('PAGE NOT FOUND: hit the 404 handler!');
   err.status = 404;
   next(err);
 });
@@ -67,7 +69,8 @@ app.use((err, req, res, next) => {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  console.log(err);
+  res.render('error', {error: err, message: "An error has occured"});
 });
 
 module.exports = app;
